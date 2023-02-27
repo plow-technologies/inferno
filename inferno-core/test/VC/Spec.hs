077eba9bab4405a6ccebc0f781e11a2a1d27ddef
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2023-02-27 06:11:33 +0000

    TEMP: try to generate realistic conc tests

--- inferno-core/test/VC/Spec.hs
+++ inferno-core/test/VC/Spec.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# LANGUAGE DataKinds #-}
 
 module Main (main) where
 
@@ -11,10 +11,9 @@ import Inferno.VersionControl.Client (api)
 import Inferno.VersionControl.Log (vcServerTraceToString)
 import qualified Inferno.VersionControl.Operations as Ops
 import Inferno.VersionControl.Server (VersionControlAPI, vcServer)
-import Network.HTTP.Client (responseStatus, Response (..), httpLbs)
+import Network.HTTP.Client (responseStatus, Response (..), httpLbs, Request, RequestBody (..), host, method, path, port, queryString, requestBody, requestHeaders, secure, defaultRequest)
+import Data.String.Conversions  (cs)
 import Network.HTTP.Types (status500)
-import Network.Wai.Logger (withStdoutLogger)
-import Network.Wai.Handler.Warp (defaultSettings, setLogger)
 import Plow.Logging (IOTracer (..), simpleStdOutTracer)
 import Servant.Server (Server)
 import Servant.QuickCheck
@@ -22,13 +21,61 @@ import Servant.QuickCheck.Internal.ErrorTypes (PredicateFailure (..))
 import Test.Hspec
 import Inferno.VersionControl.Operations.Error (VCStoreError(..))
 import GHC.Utils.TmpFs (withSystemTempDirectory)
+import System.Directory (removePathForcibly)
+import Test.QuickCheck (Result(..), quickCheckWithResult, Gen, Arbitrary (..), elements)
+import Control.Concurrent (newEmptyMVar, tryPutMVar, tryReadMVar)
+import Servant.QuickCheck.Internal.HasGenRequest (runGenRequest, HasGenRequest (..))
+import Test.QuickCheck.Monadic (monadicIO, forAllM, run, assert)
+import Servant.QuickCheck.Internal.Predicates (finishPredicates)
+import Servant.QuickCheck.Internal.QuickCheck (noCheckStatus, defManager)
+import Control.Concurrent.Async (concurrently)
+import Data.Proxy (Proxy (..))
+import Inferno.Types.Type (TCScheme)
+import Inferno.VersionControl.Types (VCObjectHash, Pinned, VCMeta)
+import Inferno.Types.Syntax (Expr)
+import Servant.API.ContentTypes (JSON (..), AllMimeRender (allMimeRender))
+import Network.HTTP.Media       (renderHeader)
+
+newtype VCApi = VCApi (VersionControlAPI Int Int)
+
+-- Test set: store successive versions of the same script, run fetchHistory for arbitrary versions
+-- Try to trigger CouldNotFindPath when fetchHistory reads wrong head
+instance HasGenRequest (VCApi) where
+  genRequest _ = (1, genStore)
+    where
+      genStore :: Gen (BaseUrl -> Request)
+      genStore = do
+        new' <- new
+        (ct, bd) <- elements $ allMimeRender (Proxy :: Proxy '[JSON]) new'
+        return $ \burl -> defaultRequest
+          { host = cs $ baseUrlHost burl
+          , port = baseUrlPort burl
+          , secure = baseUrlScheme burl == Https
+          , method = "POST"
+          , path = "/push/function"
+          , requestBody = RequestBodyLBS bd
+          , requestHeaders = ("Content-Type", renderHeader ct) : []
+          }
+        where
+          -- TODO keep a list and point to correct pred
+          -- Do we need MTL for this? See GenT
+          -- TODO limit size of Expr
+          new = arbitrary :: Gen (VCMeta Int Int (Expr (Pinned VCObjectHash) (), TCScheme))
+      genFetchHist :: Gen (BaseUrl -> Request)
+      genFetchHist = return $ \burl -> defaultRequest
+        { host = cs $ baseUrlHost burl
+        , port = baseUrlPort burl
+        , secure = baseUrlScheme burl == Https
+        , method = "GET"
+        , path = "/fetch/X8-B10K4IF4blrwGl3oztxlw-0LAeiQvkx5bxvLrr4Y="
+        }
 
 noInternalError :: RequestPredicate
 noInternalError = RequestPredicate $ \req mgr -> do
   resp <- httpLbs req mgr
   putStrLn $ show req ++ show resp
   when (responseStatus resp == status500) $ do
-    case (Aeson.decode $ responseBody resp :: Maybe VCStoreError) of
+    case ((Aeson.decode $ responseBody resp) :: Maybe VCStoreError) of
       -- HACK: these are the non-errors, which should be 404s or other codes:
       Just (CouldNotFindObject _) -> pure ()
       Just (TryingToAppendToNonHead _) -> pure ()
@@ -40,15 +87,54 @@ noInternalError = RequestPredicate $ \req mgr -> do
 
 spec :: Spec
 spec = describe "inferno-vc server" $ do
-  let maxTries = 35
+  let maxTries = 20
   let args = defaultArgs { maxSuccess = maxTries }
   -- let args = defaultArgs
 
-  it "no internal errors" $ do
-    withSystemTempDirectory "vc_store_" $ \vcPath -> do
-      withServantServer testApi (pserver vcPath) $ \url ->
-        serverSatisfies testApi url args (noInternalError <%> mempty)
-    putStrLn "Done"
+  -- it "fixed store dir" $ do
+  --   let vcPath = "vc_store"
+  --   removePathForcibly vcPath
+  --   withServantServer testApi (pserver vcPath) $ \url ->
+  --     serverSatisfies testApi url args (noInternalError <%> mempty)
+
+  -- TODO switch to using temp dir
+  -- it "no internal errors" $ do
+  --   withSystemTempDirectory "vc_store_" $ \vcPath -> do
+  --     withServantServer testApi (pserver vcPath) $ \url ->
+  --       serverSatisfies testApi url args (noInternalError <%> mempty)
+  --       -- TODO this works. Is the problem our predicate?
+  --       -- serverSatisfies testApi url args (not500 <%> mempty)
+  --   putStrLn "Done"
+
+  it "concurrent test" $ do
+    let preds = noInternalError <%> mempty
+    let vcPath = "vc_store"
+    removePathForcibly vcPath
+    let proxyApi = Proxy :: Proxy VCApi
+
+    withServantServer testApi (pserver vcPath) $ \burl -> do
+      deetsMVar <- newEmptyMVar
+      let reqs = ($ burl) <$> runGenRequest proxyApi
+      let prop = monadicIO $ forAllM reqs $ \req -> do
+            v <- run $ finishPredicates preds (noCheckStatus req) defManager
+            _ <- run $ tryPutMVar deetsMVar v
+            case v of
+              Just _ -> assert False
+              _ -> return ()
+      -- TODO do both threads generate the exact same sequence of requests?
+      (res1, res2) <- concurrently
+        (quickCheckWithResult args {chatty = False} prop)
+        (quickCheckWithResult args {chatty = False} prop)
+      -- TODO need a way to kill other threads when property violated in one thread
+      case (res1, res2) of
+        (Success {}, Success {}) -> return ()
+        _ -> do
+          mx <- tryReadMVar deetsMVar
+          case mx of
+            Just x ->
+              expectationFailure $ "Failed:\n" ++ show x
+            Nothing ->
+              expectationFailure $ "We failed to record a reason for failure: " <> show (res1, res2)
 
   where
     tracer = contramap vcServerTraceToString $ IOTracer $ simpleStdOutTracer
