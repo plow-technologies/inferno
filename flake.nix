de515b0979c424e7785498c5f459168ff2e3c301
Rory Tyler Hayford <rory.hayford@protonmail.com>
2023-03-14 14:19:24 +0700

    Allow overriding `libtorch` for CPU or CUDA

--- flake.nix
+++ flake.nix
@@ -171,16 +171,32 @@
               inputs.tokenizers.overlay
               (
                 final: prev:
-                  lib.optionalAttrs (prev.stdenv.isx86_64) rec {
+                  let
                     libtorch =
-                      final.callPackage "${inputs.hasktorch}/nix/libtorch.nix" {
-                        cudaSupport = false;
-                        device = "cpu";
-                      };
-                    torch = libtorch;
-                    c10 = libtorch;
-                    torch_cpu = libtorch;
-                  }
+                      { device ? "cpu"
+                      , cudaSupport ? final.lib.hasPrefix "cuda" device
+                      }:
+                        assert final.lib.assertOneOf "device" device
+                          [
+                            "cpu"
+                            "cuda-10"
+                            "cuda-11"
+                          ];
+                        assert device == "cpu" -> !cudaSupport;
+                        (
+                          pkgs.callPackage "${inputs.hasktorch}/nix/libtorch.nix" {
+                            inherit device cudaSupport;
+                          }
+                        ) // {
+                          passthru = { inherit device cudaSupport; };
+                        };
+                    # Adding `makeOverridable` will allow us to propagate options
+                    # from the top level (e.g. a specific `infernoFor` build) to
+                    # the package set used to build the haskell.nix project (e.g.
+                    # with or without CUDA support for the Hasktorch component)
+                    torch = pkgs.makeOverridable libtorch { device = "cpu"; };
+                  in
+                  lib.optionalAttrs prev.stdenv.isx86_64 { inherit torch; }
               )
               (_: _:
                 {
