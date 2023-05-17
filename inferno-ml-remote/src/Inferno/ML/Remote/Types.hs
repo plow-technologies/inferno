311cc437b2506198be1b2e7fa424533f31aa8aff
Rory Tyler Hayford <rory.hayford@protonmail.com>
2023-05-17 14:50:26 +0700

    Add `API` type

--- inferno-ml-remote/src/Inferno/ML/Remote/Types.hs
+++ inferno-ml-remote/src/Inferno/ML/Remote/Types.hs
@@ -1,2 +1,18 @@
--- |
-module Inferno.ML.Remote.Types () where
+{-# LANGUAGE TypeOperators #-}
+
+module Inferno.ML.Remote.Types
+  ( API,
+    infernoMlRemoteAPI,
+  )
+where
+
+import Data.Proxy (Proxy (Proxy))
+import Data.Text (Text)
+import Servant (JSON, Post, ReqBody, (:>))
+
+-- TODO
+-- Use more descriptive types. Implied `Text -> Text` is pretty awful
+type API = "inference" :> ReqBody '[JSON] Text :> Post '[JSON] Text
+
+infernoMlRemoteAPI :: Proxy API
+infernoMlRemoteAPI = Proxy
