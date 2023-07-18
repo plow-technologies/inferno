50247763640cf049dd33445dd91bd80152db8aaf
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2023-07-18 10:30:27 +0200

    Move xor test to Spec.hs

--- inferno-ml/test/Spec.hs
+++ inferno-ml/test/Spec.hs
@@ -5,7 +5,7 @@
 module Main (main) where
 
 import qualified Data.Map as Map
-import Data.Text (unpack)
+import Data.Text (Text, unpack)
 import Inferno.Core (InfernoError (..), Interpreter (evalInEnv, parseAndInfer, parseAndInferTypeReps), mkInferno)
 import Inferno.ML.Module.Prelude (mlPrelude)
 import Inferno.ML.Types.Value (MlValue (VTensor))
@@ -19,6 +19,27 @@ main :: IO ()
 main =
   hspec $ do
     evalTests
+
+xorScript :: Text
+xorScript =
+  "\
+  \ open ML in                                                                         \n\
+  \ // These are the parameters from a model trained offline with hasktorch + Haskell: \n\
+  \ let w1 = asTensor2 #float [[-1.1163   ,  1.1485   ], [-1.5528   ,  1.6075   ]] in  \n\
+  \ let b1 = asTensor1 #float [-0.5647   ,  0.8927   ] in                              \n\
+  \ let w2 = asTensor2 #float [[ 1.2794   , -1.1922   ]] in                            \n\
+  \ let b2 = asTensor1 #float [ 1.5040   ] in                                          \n\
+  \ // This is an inferno function that runs inference using the trained model:        \n\
+  \ let mlp = fun x ->                                                                 \n\
+  \   let y1 = add (matmul x (transpose2D w1)) b1 in                                   \n\
+  \   let y2 = add (matmul (tanH y1) (transpose2D w2)) b2 in                           \n\
+  \   y2                                                                               \n\
+  \ in                                                                                 \n\
+  \ // Let's check that it computes XOR correctly on some inputs:                      \n\
+  \ let inputs = Array.map (asTensor1 #float) [ [0, 0], [0, 1], [1, 0], [1, 1]]        \n\
+  \ in                                                                                 \n\
+  \ Array.map round (Array.map asDouble (Array.map mlp inputs))                        \n\
+  \"
 
 evalTests :: Spec
 evalTests = describe "evaluate" $
@@ -34,6 +55,7 @@ evalTests = describe "evaluate" $
             T.asTensor [[1, 2, 4 :: Double]]
     shouldFailToInferTypeFor "open ML in asTensor4 #float [[1, 2, 4]]"
     shouldEvaluateTo "open ML in asDouble (sumAll (ones #int [2, 4]))" $ VDouble 8.0
+    shouldEvaluateTo xorScript $ VArray (map VInt [0, 1, 1, 0])
   where
     inferno = mkInferno mlPrelude :: Interpreter MlValue
     shouldEvaluateInEnvTo localEnv implEnv str (v :: Value MlValue IO) =
