a7527f82aea1f54166d94721b7f653d59ecb5a62
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2023-09-12 10:13:43 +0000

    Add black box tests using HTF

--- inferno-core/test/Spec.hs
+++ inferno-core/test/Spec.hs
@@ -5,9 +5,14 @@ import Golden.Spec (goldenTests)
 import Infer.Spec (inferTests)
 import Parse.Spec (parsingTests)
 import Test.Hspec (hspec)
+import Test.Framework ( htfMain, makeTestSuite )
+import Test.Framework.BlackBoxTest ( blackBoxTests, defaultBBTArgs )
 
 main :: IO ()
-main =
+main = do
+  bbts <- blackBoxTests "." "/home/sid/inferno/dist-newstyle/build/x86_64-linux/ghc-9.2.5/inferno-core-0.4.0.0/x/inferno/build/inferno/inferno" ".inferno" defaultBBTArgs
+  htfMain [makeTestSuite "bbts" bbts]
+
   hspec $ do
     parsingTests
     inferTests
