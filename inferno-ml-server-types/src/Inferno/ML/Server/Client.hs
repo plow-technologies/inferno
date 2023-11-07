753fec60daeb1b8f6b4983f7869a680d47fa46b9
Rory Tyler Hayford <rory.hayford@protonmail.com>
2023-11-07 11:37:23 +0700

    Update client

--- inferno-ml-server-types/src/Inferno/ML/Server/Client.hs
+++ inferno-ml-server-types/src/Inferno/ML/Server/Client.hs
@@ -5,13 +5,14 @@ module Inferno.ML.Server.Client where
 
 import Conduit (ConduitT)
 import Data.Proxy (Proxy (Proxy))
+import Data.Scientific (Scientific)
 import Inferno.ML.Server.Types
 import Servant.Client.Streaming (ClientM, client)
 
 runInference ::
   forall uid gid.
   InferenceRequest uid gid ->
-  ClientM (ConduitT () AsValue IO ())
+  ClientM (ConduitT () (AsValue Scientific) IO ())
 runInference = client api
 
 api :: Proxy (InfernoMlServerAPI uid gid)
