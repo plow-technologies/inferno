699d328421df31969116a6673f4b9d8c258ece60
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2024-03-05 11:38:50 +0200

    Add CLI options to inferno exe

--- inferno-core/app/Main.hs
+++ inferno-core/app/Main.hs
@@ -3,36 +3,68 @@
 
 module Main where
 
+import Data.Bifunctor (bimap)
 import qualified Data.Map as Map
 import qualified Data.Text.IO as Text
 import Inferno.Core (Interpreter (..), mkInferno)
 import Inferno.Module.Prelude (builtinModules)
+import Inferno.Types.VersionControl (pinnedToMaybe)
 import Inferno.Utils.Prettyprinter (showPretty)
-import System.Environment (getArgs)
+import Options.Applicative
+  ( Parser,
+    argument,
+    execParser,
+    fullDesc,
+    header,
+    help,
+    helper,
+    info,
+    long,
+    metavar,
+    progDesc,
+    short,
+    str,
+    switch,
+    (<**>),
+  )
 import System.Exit (exitFailure)
-import System.IO (hPutStrLn, stderr)
+import System.IO (hPrint, stderr)
+
+data CliArgs = CliArgs {file :: String, typecheck :: Bool}
+
+cliargs :: Parser CliArgs
+cliargs =
+  CliArgs
+    <$> argument str (metavar "FILE" <> help "Input file path")
+    <*> switch (long "typecheck" <> short 't' <> help "Only run type inference")
 
 main :: IO ()
 main = do
-  file <- head <$> getArgs
-  src <- Text.readFile file
-  Interpreter {evalExpr, defaultEnv, parseAndInferTypeReps, parseAndInfer} <-
+  let opts =
+        info
+          (cliargs <**> helper)
+          ( fullDesc
+              <> progDesc "Run Inferno on FILE"
+              <> header "inferno - a functional scripting language"
+          )
+  args <- execParser opts
+
+  src <- Text.readFile $ file args
+  Interpreter {evalExpr, defaultEnv, parseAndInfer} <-
     mkInferno builtinModules [] :: IO (Interpreter IO ())
   case parseAndInfer src of
     Left err -> do
-      hPutStrLn stderr $ show err
+      hPrint stderr err
       exitFailure
     Right (ast, ty, _, _) -> do
-      putStrLn "Inferred type:"
-      showPretty ty
-
--- case parseAndInferTypeReps src of
---   Left err -> do
---     hPutStrLn stderr $ show err
---     exitFailure
---   Right ast -> do
---     evalExpr defaultEnv Map.empty ast >>= \case
---       Left err -> do
---         hPutStrLn stderr $ show err
---         exitFailure
---       Right res -> showPretty res
+      if typecheck args
+        then do
+          putStrLn "Inferred type:"
+          showPretty ty
+        else do
+          let ast' = bimap pinnedToMaybe (const ()) ast
+          evalExpr defaultEnv Map.empty ast' >>= \case
+            Left err -> do
+              hPrint stderr err
+              exitFailure
+            Right res -> showPretty res
