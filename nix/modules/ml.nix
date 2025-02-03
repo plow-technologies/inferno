40e9be562f32ba92337ceca29118ddd1f87a5e1f
Rory Tyler Hayford <rory.hayford@protonmail.com>
2025-02-03 15:34:42 +0700

    More config for Hasktorch things

--- nix/modules/ml.nix
+++ nix/modules/ml.nix
@@ -3,29 +3,29 @@
 {
   packages = {
     # TODO upgrade re-enable
-    # libtorch-ffi = {
-    #   configureFlags = [
-    #     "--extra-lib-dirs=${pkgs.torch.out}/lib"
-    #     "--extra-include-dirs=${lib.getDev pkgs.torch.dev}/include/torch/csrc/api/include"
-    #   ];
-    #   flags = {
-    #     cuda = cudaSupport;
-    #     gcc = !cudaSupport && pkgs.stdenv.hostPlatform.isDarwin;
-    #     # Flag for linking torch platform for AMD GPUs
-    #     #
-    #     # This is also hardcoded to `false` in Hasktorch's own haskell.nix
-    #     # configuration, so I'm not sure if it's even supported
-    #     # See:
-    #     # https://github.com/hasktorch/hasktorch/blob/de3b709980a0d78d2284d91847c09f522830da61/nix/haskell.nix
-    #     rocm = false;
-    #   };
-    # };
+    libtorch-ffi = {
+      configureFlags = [
+        "--extra-lib-dirs=${pkgs.torch.out}/lib"
+        "--extra-include-dirs=${lib.getDev pkgs.torch.dev}/include"
+      ];
+      flags = {
+        cuda = cudaSupport;
+        gcc = !cudaSupport && pkgs.stdenv.hostPlatform.isDarwin;
+        # Flag for linking torch platform for AMD GPUs
+        #
+        # This is also hardcoded to `false` in Hasktorch's own haskell.nix
+        # configuration, so I'm not sure if it's even supported
+        # See:
+        # https://github.com/hasktorch/hasktorch/blob/de3b709980a0d78d2284d91847c09f522830da61/nix/haskell.nix
+        rocm = false;
+      };
+    };
 
-    # tokenizers = {
-    #   configureFlags = [
-    #     "--extra-lib-dirs=${pkgs.tokenizers-haskell}/lib"
-    #   ];
-    # };
+    tokenizers = {
+      configureFlags = [
+        "--extra-lib-dirs=${pkgs.tokenizers-haskell}/lib"
+      ];
+    };
   };
 
   # TODO upgrade Do we need any of these?
