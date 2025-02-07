d668801081d8e153b4c510df804868833dbdc145
Rory Tyler Hayford <rory.hayford@protonmail.com>
2025-02-07 11:28:24 +0700

    Use correct libtorch version

--- nix/overlays/torch.nix
+++ nix/overlays/torch.nix
@@ -8,41 +8,40 @@ let
     # We only care about supporting this platform for now. Darwin support
     # isn't necessary
       assert system == "x86_64-linux";
-      # While we could support CUDA 11.8, we can't get NVIDIA drivers for it
-      # apparently (for V100 GPUs)
       assert lib.assertOneOf "device" device [ "cpu" "cuda-118" ];
       assert device == "cpu" -> !cudaSupport;
 
-      (
-        prev.libtorch-bin.override { inherit cudaSupport; }).overrideAttrs
+      (prev.libtorch-bin.override { inherit cudaSupport; }).overrideAttrs
         (
-          old: rec {
+          old:
+          let
             version = "2.3.0";
-            src =
-              let
-                baseUrl = "https://download.pytorch.org/libtorch";
-                getTorch =
-                  { # Compute architecture, e.g. CPU or CUDA. But this is
-                    # formatted a different way than the `device` above to
-                    # antiquote correctly into the Pytorch URL
-                    arch
-                  , hash
-                  }: prev.fetchzip {
-                    inherit hash;
-                    name = "libtorch-cxx11-abi-shared-with-deps-${version}-${device}.zip";
-                    url = "${baseUrl}/${arch}/libtorch-cxx11-abi-shared-with-deps-${version}%2B${arch}.zip";
-                  };
-              in
-                {
-                  cpu = getTorch {
-                    arch = "cpu";
-                    hash = "sha256-dKAk6UusK2eQIcP0oMXh9cnufMpy5Ph4SGPkIPPV6ds=";
-                  };
-                  cuda-118 = getTorch {
-                    arch = "cu118";
-                    hash = "sha256-0000000000000000000000000000000000000000000=";
-                  };
-                }.${device} or (throw "Invalid device");
+            baseUrl = "https://download.pytorch.org/libtorch";
+            getTorch =
+              {
+                # Compute architecture, e.g. CPU or CUDA. But this is
+                # formatted a different way than the `device` above to
+                # antiquote correctly into the Pytorch URL
+                arch
+              , hash
+              }: prev.fetchzip {
+                inherit hash;
+                name = "libtorch-cxx11-abi-shared-with-deps-${version}-${device}.zip";
+                url = "${baseUrl}/${arch}/libtorch-cxx11-abi-shared-with-deps-${version}%2B${arch}.zip";
+              };
+          in
+          {
+            inherit version;
+            src = {
+              cpu = getTorch {
+                arch = "cpu";
+                hash = "sha256-dKAk6UusK2eQIcP0oMXh9cnufMpy5Ph4SGPkIPPV6ds=";
+              };
+              cuda-118 = getTorch {
+                arch = "cu118";
+                hash = "sha256-/rEdYSGh+2omYqmdH79vrQfW5H52jAA7AkmTYdxHnhU=";
+              };
+            }.${device} or (throw "Invalid device");
           }
         );
 
