a986f4215d00ef52f827ff35617f7c6a74b7bb90
Rory Tyler Hayford <rory.hayford@protonmail.com>
2025-06-27 17:43:24 +0700

    WIP: Start adding primitives to Inferno modules

--- inferno-ml-compat/src/Inferno/ML/Module/Compat.hs
+++ inferno-ml-compat/src/Inferno/ML/Module/Compat.hs
@@ -248,11 +248,26 @@ module ML
 
   enum device := #cpu | #cuda;
 
+  @doc Load a named, serialized model;
+  loadModel : modelName -> model := ###!loadModel###;
+
+  unsafeLoadScript : text -> model := ###unsafeLoadScript###;
+
+  forward : model -> array of tensor -> array of tensor := ###!forward###;
+
   zeros : dtype{#int, #float, #double} -> array of int -> tensor := ###!zeros###;
 
   ones : dtype{#int, #float, #double} -> array of int -> tensor := ###!ones###;
 
-  add : tensor -> tensor -> tensor := ###add###;
+  @doc An impure (pseudo)random tensor generator;
+  randnIO : dtype{#int, #float, #double} -> array of int -> tensor := ###!randnIO###;
+
+  @doc Move a tensor to a different device;
+  toDevice : device{#cpu, #cuda} -> tensor -> tensor := ###!toDevice###;
+
+  @doc Move a tensor to a different device, e.g. "cpu" or "cuda:0"
+  (without checking validity of device name);
+  toDeviceUnsafe : text -> tensor -> tensor := ###toDeviceUnsafe###;
 
   toType : dtype{#int, #float, #double} -> tensor -> tensor := ###!toType###;
 
@@ -276,43 +291,146 @@ module ML
 
   asArray4 : tensor -> array of (array of (array of (array of double))) := ###asArray4###;
 
+module Tensor
+
+  @doc Returns the mean value of all elements in the input tensor;
+  mean : tensor -> tensor := ###mean###;
+
+  @doc Returns the standard deviation of all elements in the input tensor;
+  std : tensor -> tensor := ###std###;
+
+  @doc Returns the variance of all elements in the input tensor;
+  var : tensor -> tensor := ###var###;
+
+  @doc Returns the sum of all elements in the input tensor;
   sumAll : tensor -> tensor := ###sumAll###;
 
-  powt : int -> tensor -> tensor := ###powt###;
+  @doc Computes the element-wise absolute value of the given input tensor;
+  abs : tensor -> tensor := ###abs###;
 
-  tanH : tensor -> tensor := ###tanh###;
+  @doc Computes the fractional portion of each element in input.
+  `output = input - (floor . abs) input * (sign input)`;
+  frac : tensor -> tensor := ###frac###;
 
-  @doc `argmax i k t` is the argmax of tensor `t` along dimension `i`. `k` denotes whether the output tensor has dim retained or not.;
+  @doc `argmax i k t` is the argmax of tensor `t` along dimension `i`.
+  `k` denotes whether the output tensor has dim retained or not.;
   argmax : int -> bool{#true, #false} -> tensor -> tensor := ###argmax###;
+
+  @doc `add t1 t2` Each element of the tensor `t2` added to each element of the tensor `t1`.
+  The resulting tensor is returned;
+  add : tensor -> tensor -> tensor := ###add###;
+
+  @doc `mul t1 t2` multiplies each element of the tensor `t2` to each element of the
+  input tensor `t1` and returns a new resulting tensor;
+  mul : tensor -> tensor -> tensor := ###mul###;
+
+  @doc `sub t1 t2` performs element wise subtraction of tensor `t2` from input tensor
+  `t1` and returns a new resulting tensor;
+  sub : tensor -> tensor -> tensor := ###sub###;
+
+  @doc `div t1 t2` performs element wise division of tensor `t2` from input tensor
+  `t1` and returns a new resulting tensor;
+  div : tensor -> tensor -> tensor := ###div###;
+
+  @doc Returns a new tensor with the ceil of the elements of input, the smallest
+  integer greater than or equal to each element;
+  ceil : tensor -> tensor -> tensor := ###ceil###;
+
+  @doc Returns a new tensor with the floor of the elements of input, the smallest
+  integer less than or equal to each element;
+  floor : tensor -> tensor -> tensor := ###floor###;
+
+  @doc Returns the minimum value of all elements in the input tensor;
+  min : tensor -> tensor -> tensor := ###min###;
+
+  @doc Returns the maximum value of all elements in the input tensor;
+  max : tensor -> tensor -> tensor := ###max###;
+
+  @doc Returns the median value of all elements in the input tensor;
+  median : tensor -> tensor -> tensor := ###median###;
+
+  @doc `matmul t1 t2` is the matrix product of two tensors. The behavior depends
+  on the dimensionality of the tensors as follows:
+    - If both tensors are 1-dimensional, the dot product (scalar) is returned
+    - If both arguments are 2-dimensional, the matrix-matrix product is returned
+    - If `t1` is 1-dimensional and `t2` is 2-dimensional, a 1 is prepended to
+      its dimension for the purpose of the matrix multiply. After the matrix
+      multiply, the prepended dimension is removed
+    - If `t1` is 2-dimensional and the `t2` is 1-dimensional, the matrix-vector product is returned
+    - If both arguments are at least 1-dimensional and at least one argument is
+      N-dimensional (where N > 2), then a batched matrix multiply is returned;
+  matmul : tensor -> tensor -> tensor := ###matmul###;
+
+  @doc `oneHot i t` is one hot encoding of the given input `t`. The encoding is
+  based on the given number of classes `i`;
+  oneHot : int -> tensor -> tensor := ###oneHot###;
+
+  @doc Computes the error function of each element;
+  erf : tensor -> tensor := ###erf###;
+
+  @doc Computes the complementary error function of each element;
+  erfc : tensor -> tensor := ###erfc###;
+
+  @doc Computes the inverse error function of each element of input.
+  The inverse error function is defined in the range `(-1, 1)(−1,1)` as:
+  `erfinv(erf(x)) = x`;
+  erfinv : tensor -> tensor := ###erfinv###;
+
+  @doc Computes the logarithm of the gamma function on input;
+  lgamma : tensor -> tensor := ###lgamma###;
+
+  @doc Computes the logarithmic derivative of the gamma function on input;
+  digamma : tensor -> tensor := ###digamma###;
+
+  @doc `polygamma n t` computes the `n`th derivative of the digamma function on
+  input. n≥0n≥0 is called the order of the polygamma function;
+  polygamma : int -> tensor -> tensor := ###polygamma###;
+
+  @doc `mvlgamma p t` computes the multivariate log-gamma function with dimension
+  `p` element-wise. All elements must be greater than `(p-1)/2`, otherwise an
+  error would be thrown;
+  mvlgamma : int -> tensor -> tensor := ###mvlgamma###;
+
+  @doc Returns a new tensor with the exponential of the elements of the input tensor;
+  exp : tensor -> tensor := ###exp###;
+
+  @doc `log1p t` returns a new tensor with the natural logarithm of `(1 + t)`;
+  log1p : tensor -> tensor := ###log1p###;
+
+  @doc Returns a new tensor with the logarithm to the base 2 of the elements of input;
+  log2 : tensor -> tensor := ###log2###;
+
+  @doc Returns a new tensor with the logarithm of the elements of input;
+  log : tensor -> tensor := ###log###;
+
+  @doc Returns a new tensor with the logarithm to the base 10 of the elements of input;
+  log10 : tensor -> tensor := ###log10###;
+
+  @doc `pow e t` takes the power of each element in `t` with exponent `e` and
+  returns a tensor with the result;
+  pow : int -> tensor -> tensor := ###pow###;
+
+  @doc `powt t1 t2` takes the power of each element in input `t1` with exponent
+  `t2` and returns a tensor with the result. Exponent `t2` is a tensor with the
+  same number of elements as the input;
+  powt : tensor -> tensor -> tensor := ###powt###;
+
+  @doc Applies the rectified linear unit function element-wise;
+  relu : tensor -> tensor := ###relu###;
+
+  tanh : tensor -> tensor := ###tanh###;
 
   softmax : int -> tensor -> tensor := ###softmax###;
 
-  @doc `stack i [t]` takes an array of tensors `t` and appends them along the dimension i in a new tensor;
+  @doc `stack i [t]` takes an array of tensors `t` and appends them along the
+  dimension `i` in a new tensor;
   stack : int -> array of tensor -> tensor := ###stack###;
 
   transpose2D : tensor -> tensor := ###transpose2D###;
 
-  matmul : tensor -> tensor -> tensor := ###matmul###;
-
   mseLoss : tensor -> tensor -> tensor := ###mseLoss###;
 
   unsqueeze : int -> tensor -> tensor := ###unsqueeze###;
-
-  @doc An impure (pseudo)random tensor generator;
-  randnIO : dtype{#int, #float, #double} -> array of int -> tensor := ###!randnIO###;
-
-  @doc Move a tensor to a different device;
-  toDevice : device{#cpu, #cuda} -> tensor -> tensor := ###!toDevice###;
-
-  @doc Move a tensor to a different device, e.g. "cpu" or "cuda:0" (without checking validity of device name);
-  toDeviceUnsafe : text -> tensor -> tensor := ###toDeviceUnsafe###;
-
-  @doc Load a named, serialized model;
-  loadModel : modelName -> model := ###!loadModel###;
-
-  unsafeLoadScript : text -> model := ###unsafeLoadScript###;
-
-  forward : model -> array of tensor -> array of tensor := ###!forward###;
 
 |]
   where
