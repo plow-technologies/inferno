6317d6fddf585430749350dd1ba908429d913294
Rory Tyler Hayford <rory.hayford@protonmail.com>
2025-09-23 15:05:47 +0700

    Restart the process when memory limit is breached

--- inferno-ml-server/src/Inferno/ML/Server/Utils.hs
+++ inferno-ml-server/src/Inferno/ML/Server/Utils.hs
@@ -43,15 +43,17 @@ import Database.PostgreSQL.Simple.Vector (query)
 import Inferno.ML.Server.Types
 import Inferno.VersionControl.Types (VCObjectHash)
 import Lens.Micro.Platform (view)
+import System.Exit (ExitCode (ExitFailure))
 import System.FilePath ((</>))
 import System.IO.Error
   ( isEOFError,
     isIllegalOperation,
     isResourceVanishedError,
   )
+import System.Posix.Process (exitImmediately)
 import UnliftIO (MonadUnliftIO (withRunInIO))
 import UnliftIO.Async (cancel, waitEitherCatch, withAsync)
-import UnliftIO.Concurrent (threadDelay)
+import UnliftIO.Concurrent (forkIO, threadDelay)
 import UnliftIO.Directory (doesPathExist)
 import UnliftIO.Exception
   ( SomeException,
@@ -59,6 +61,8 @@ import UnliftIO.Exception
     catchJust,
     displayException,
     fromException,
+    mask_,
+    throwIO,
   )
 import UnliftIO.IO
   ( Handle,
@@ -108,7 +112,8 @@ withConns f = view typed >>= \cs -> withRunInIO $ \r -> withResource cs $ r . f
 -- | Try to run a @RemoteM@ process while monitoring its memory usage. If
 -- the value of the process @memory.current@ comes within a close threshold of
 -- the server\'s @MemoryMax@ acccording to its systemd service configuration,
--- the action is cancelled and an exception is raised
+-- the action is cancelled and an exception is raised. The server process is
+-- restarted to reclaim memory
 --
 -- NOTE: If the cgroup @memory.max@ information is missing or unreadable at the
 -- time of server start, the provided @RemoteM@ action is run /without/ memory
@@ -116,10 +121,8 @@ withConns f = view typed >>= \cs -> withRunInIO $ \r -> withResource cs $ r . f
 --
 -- Even without the active monitor below, if the entire server process exceeds
 -- @MemoryMax@ it will be stopped and restarted. However, it\'s preferable to
--- stop a single action within the server and keep the server running rather
--- than stopping and re-starting the entire process. First, we can still service
--- other endpoints, e.g. @/status@. Second, we can return a more meaningful
--- error message
+-- first return the response with the exception as the response body for a more
+-- meaningful error message
 --
 -- NOTE: This is really only meant to be used with the script evaluator, which
 -- is the only server endpoint that can consume a significant amount of memory.
@@ -149,6 +152,19 @@ withMemoryMonitor f =
                   -- cancel `f` and re-throw the exception
                   Left (Left e) -> do
                     cancel inner
+                    -- Schedule a restart for 500ms from now, giving enough time
+                    -- for warp handler to send the response with the correct body
+                    -- (i.e. the synchronously rethrown exception below). Exit
+                    -- code of 1 for generic non-success
+                    --
+                    -- NOTE: We do a restart here because even try to force
+                    -- memory reclamation both Torch FFI and Haskell GC does
+                    -- not seem to free enough memory. Then we are stuck in
+                    -- a position where the memory monitor will fail, but we
+                    -- don't get restarted by systemd. However, here we can send
+                    -- the response with a clear body describing the problem and
+                    -- release the memory by killing the process
+                    scheduleSelfRestart 500000 1
                     throwRemoteError $ toRemoteError e
                   -- Should never happen; monitor runs in infinite loop until it
                   -- throws an exception
@@ -192,7 +208,8 @@ withMemoryMonitor f =
               -- which propagates to the calling thread, which also includes the
               -- `RemoteM` effect in its child scope, i.e. also killing it
               when (current >= limit) $
-                throwRemoteError $ MemoryLimitExceeded memMax current
+                throwRemoteError $
+                  MemoryLimitExceeded memMax current
           -- Wait 200ms
           threadDelay 200000
           where
@@ -238,13 +255,14 @@ withMemoryMonitor f =
 -- | Get the maximum memory in bytes that the server process can use according
 -- to its systemd policy. Note:
 --   * if the cgroup @memory.max@ does not exist, @Nothing@ is returned
---   * unparseable contents such as @infinity@, etc... will result in @Nothing@
+--   * unparseable contents such as @infinity@, @max@ etc... also return @Nothing@
 --
 -- This is a constant value and depends on the value of the @MemoryMax@ defined
 -- for the server\'s systemd service
 getMemoryMax :: (MonadIO m) => m (Maybe Word64)
 getMemoryMax = getCgroupMemInfo "memory.max"
 
+-- | Try to parse memory in bytes from a file under the server cgroup directory
 getCgroupMemInfo ::
   (MonadIO m) =>
   -- | Path to relevant file in cgroup directory, e.g. @memory.max@, @memory.current@
@@ -271,3 +289,20 @@ cgroupPath = "/sys/fs/cgroup/system.slice/inferno-ml-server.service"
 -- log a warning if we just restarted the server due to an OOM kill
 lastOomPath :: FilePath
 lastOomPath = "/var/lib/inferno-ml-server/last-oom"
+
+-- | Force a restart of the entire server process internally. This forks a
+-- thread that waits for the given amount of microseconds before exiting
+-- immediately.
+--
+-- NOTE: Exit code MUST be non-zero for systemd to restart service
+scheduleSelfRestart :: (MonadUnliftIO m) => Int -> Int -> m ()
+scheduleSelfRestart micros code
+  | code <= 0 = throwIO $ userError "`scheduleRestart`: exit code must be non-zero"
+  | otherwise =
+      -- NOTE: `mask_` here because we DON'T want to be killed by any async
+      -- exceptions. This is meant to kill the process without any interruption
+      mask_ . void . forkIO $ do
+        -- Delaying gives Warp the necessary amount of time for the response to
+        -- be sent
+        threadDelay micros
+        liftIO . exitImmediately $ ExitFailure code
