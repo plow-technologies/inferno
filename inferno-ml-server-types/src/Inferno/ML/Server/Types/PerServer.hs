d16fd67ef5a0932c71a8231e3fc84a4e689ede6f
Rory Tyler Hayford <rory.hayford@protonmail.com>
2025-10-15 15:46:34 +0700

    Do not bother with aggravation of broken `LogLevel` serde

--- inferno-ml-server-types/src/Inferno/ML/Server/Types/PerServer.hs
+++ inferno-ml-server-types/src/Inferno/ML/Server/Types/PerServer.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TypeOperators #-}
 {-# OPTIONS_GHC -Wno-unticked-promoted-constructors #-}
@@ -11,15 +10,15 @@ module Inferno.ML.Server.Types.PerServer where
 
 import Data.Aeson
   ( FromJSON (parseJSON),
-    ToJSON,
-    withObject,
-    (.!=),
-    (.:),
-    (.:?),
+    ToJSON (toJSON),
+    camelTo2,
+    defaultOptions,
+    fieldLabelModifier,
+    genericParseJSON,
+    genericToJSON,
   )
 import Data.Text (Text)
 import GHC.Generics (Generic)
-import Plow.Logging.Message (LogLevel (LevelInfo))
 import Servant
   ( Get,
     JSON,
@@ -36,18 +35,23 @@ type PerServerAPI =
   "inferno-ml" :> "configure" :> "set" :> ReqBody '[JSON] PerServerConfig :> Post '[JSON] ()
     :<|> "inferno-ml" :> "configure" :> "get" :> Get '[JSON] PerServerConfig
 
-data PerServerConfig = PerServerConfig
+newtype PerServerConfig = PerServerConfig
   { instanceId :: Text
   -- ^ ID of the instance that @inferno-ml-server@ is running on. This is
   -- needed for tracing to the DB
-  , logLevel :: LogLevel
-  -- ^ Minimum log level for this instance
   }
   deriving stock (Show, Eq, Generic)
-  deriving anyclass (ToJSON)
 
 instance FromJSON PerServerConfig where
-  parseJSON = withObject "PerServerConfig" $ \o ->
-    PerServerConfig
-      <$> o .: "instance-id"
-      <*> o .:? "log-level" .!= LevelInfo
+  parseJSON =
+    genericParseJSON
+      defaultOptions
+        { fieldLabelModifier = camelTo2 '-'
+        }
+
+instance ToJSON PerServerConfig where
+  toJSON =
+    genericToJSON
+      defaultOptions
+        { fieldLabelModifier = camelTo2 '-'
+        }
