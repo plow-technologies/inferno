624cdd8d42a3d5d1f974190f2ed4251cea1697d9
Rory Tyler Hayford <rory.hayford@protonmail.com>
2025-10-15 16:03:12 +0700

    Actually fixed cursed `LogLevel`

--- inferno-ml-server-types/src/Inferno/ML/Server/Types/PerServer.hs
+++ inferno-ml-server-types/src/Inferno/ML/Server/Types/PerServer.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE TypeOperators #-}
 {-# OPTIONS_GHC -Wno-unticked-promoted-constructors #-}
 
@@ -8,17 +9,22 @@
 -- from the general, universal settings for Inferno ML images
 module Inferno.ML.Server.Types.PerServer where
 
+import Data.Maybe (fromMaybe)
+import Data.Char (toLower)
 import Data.Aeson
   ( FromJSON (parseJSON),
+    Object,
     ToJSON (toJSON),
-    camelTo2,
-    defaultOptions,
-    fieldLabelModifier,
-    genericParseJSON,
-    genericToJSON,
+    object,
+    withObject,
+    (.=),
+    (.:),
+    (.:?),
   )
+import Data.Aeson.Types (Parser)
 import Data.Text (Text)
 import GHC.Generics (Generic)
+import Plow.Logging.Message (LogLevel (LevelInfo, LevelWarn, LevelError))
 import Servant
   ( Get,
     JSON,
@@ -35,23 +41,40 @@ type PerServerAPI =
   "inferno-ml" :> "configure" :> "set" :> ReqBody '[JSON] PerServerConfig :> Post '[JSON] ()
     :<|> "inferno-ml" :> "configure" :> "get" :> Get '[JSON] PerServerConfig
 
-newtype PerServerConfig = PerServerConfig
+data PerServerConfig = PerServerConfig
   { instanceId :: Text
   -- ^ ID of the instance that @inferno-ml-server@ is running on. This is
   -- needed for tracing to the DB
+  , logLevel :: LogLevel
+  -- ^ Minimum log level for this instance
   }
   deriving stock (Show, Eq, Generic)
 
 instance FromJSON PerServerConfig where
-  parseJSON =
-    genericParseJSON
-      defaultOptions
-        { fieldLabelModifier = camelTo2 '-'
-        }
+  parseJSON = withObject "PerServerConfig" $ \o ->
+    PerServerConfig
+      <$> o .: "instance-id"
+      <*> fmap (fromMaybe LevelInfo) (logLevelP o)
+    where
+      logLevelP :: Object -> Parser (Maybe LogLevel)
+      logLevelP o =
+        o .:? "log-level" >>= \case
+          Just i@"info" -> pure $ toLogLevel i
+          Just i@"warn" -> pure $ toLogLevel i
+          Just i@"error" -> pure $ toLogLevel i
+          _ -> pure Nothing
+
+      toLogLevel :: Text -> Maybe LogLevel
+      toLogLevel = \case
+        "info" -> pure LevelInfo
+        "warn" -> pure LevelWarn
+        "error" -> pure LevelError
+        _ -> Nothing
+
 
 instance ToJSON PerServerConfig where
-  toJSON =
-    genericToJSON
-      defaultOptions
-        { fieldLabelModifier = camelTo2 '-'
-        }
+  toJSON cfg =
+    object
+      [ "instance-id" .= cfg.instanceId
+      , "log-level" .= toJSON (fmap toLower (show cfg.logLevel))
+      ]
