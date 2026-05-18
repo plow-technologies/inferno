fee69dc8e9e974eb1af8f19f241d4f4ad9a74a3f
Rory Tyler Hayford <rory.hayford@protonmail.com>
2026-05-18 13:30:19 +0700

    Add `DocState` and helpers

--- inferno-lsp/src/Inferno/LSP/DocState.hs
+++ inferno-lsp/src/Inferno/LSP/DocState.hs
@@ -1,1 +1,116 @@
-module Inferno.LSP.DocState where
+{-# LANGUAGE NoFieldSelectors #-}
+
+module Inferno.LSP.DocState
+  ( DocState
+      ( DocState,
+        version,
+        hoverIdx,
+        typeMap,
+        classes,
+        analysis,
+        hoverCache
+      ),
+    AnalysisResult
+      ( AnalysisResult
+      ),
+    DocStore,
+    lookupDoc,
+    insertDoc,
+    deleteDoc,
+    cancelAnalysis,
+    closeDoc,
+    updateDoc,
+    setAnalysis,
+    openDoc,
+    newDoc,
+  ) where
+
+import Data.Foldable (traverse_) -- NOTE: Do NOT remove, needed for GHC version compat
+import Data.Int (Int32)
+import Data.Map.Strict (Map)
+import Data.Set (Set)
+import Data.Word (Word32)
+import Inferno.LSP.Hover (HoverIndex)
+import Inferno.Types.Syntax (TCScheme, TypeClass, TypeMetadata)
+import Language.LSP.Types (MarkupContent, NormalizedUri, Range)
+import qualified StmContainers.Map
+import Text.Megaparsec.Pos (SourcePos)
+import UnliftIO.Async (Async, cancel)
+import UnliftIO.STM
+  ( STM,
+    TVar,
+    atomically,
+    modifyTVar',
+    newTVar,
+    readTVarIO,
+    writeTVar,
+  )
+
+data DocState = DocState
+  { version :: {-# UNPACK #-} !Int32
+  , hoverIdx :: !HoverIndex
+  , typeMap :: !(Map (SourcePos, SourcePos) (TypeMetadata TCScheme))
+  , classes :: !(Set TypeClass)
+  , analysis :: !(Maybe (Async ()))
+  , hoverCache :: !(Map (Word32, Word32) (Range, MarkupContent))
+  }
+
+type DocStore = StmContainers.Map.Map NormalizedUri (TVar DocState)
+
+lookupDoc :: NormalizedUri -> DocStore -> STM (Maybe (TVar DocState))
+lookupDoc = StmContainers.Map.lookup
+
+insertDoc :: NormalizedUri -> TVar DocState -> DocStore -> STM ()
+insertDoc uri tvar = StmContainers.Map.insert tvar uri
+
+deleteDoc :: NormalizedUri -> DocStore -> STM ()
+deleteDoc = StmContainers.Map.delete
+
+cancelAnalysis :: TVar DocState -> IO ()
+cancelAnalysis tvar = traverse_ cancel . (.analysis) =<< readTVarIO tvar
+
+closeDoc :: NormalizedUri -> DocStore -> IO ()
+closeDoc uri store =
+  traverse_ ((*> atomically (deleteDoc uri store)) . cancelAnalysis)
+    =<< atomically (lookupDoc uri store)
+
+data AnalysisResult = AnalysisResult
+  { version :: {-# UNPACK #-} !Int32
+  , hoverIdx :: !HoverIndex
+  , typeMap :: !(Map (SourcePos, SourcePos) (TypeMetadata TCScheme))
+  , classes :: !(Set TypeClass)
+  }
+
+updateDoc :: AnalysisResult -> TVar DocState -> STM ()
+updateDoc res tvar =
+  writeTVar
+    tvar
+    DocState
+      { version = res.version
+      , hoverIdx = res.hoverIdx
+      , typeMap = res.typeMap
+      , classes = res.classes
+      , analysis = Nothing
+      , hoverCache = mempty
+      }
+
+setAnalysis :: Async () -> TVar DocState -> STM ()
+setAnalysis a tvar = modifyTVar' tvar $ \ds -> ds{analysis = Just a}
+
+openDoc :: NormalizedUri -> DocStore -> STM (TVar DocState)
+openDoc uri store =
+  newDoc >>= \doc ->
+    doc <$ insertDoc uri doc store
+
+
+newDoc :: STM (TVar DocState)
+newDoc =
+  newTVar
+    DocState
+      { version = 0
+      , hoverIdx = mempty
+      , typeMap = mempty
+      , classes = mempty
+      , analysis = Nothing
+      , hoverCache = mempty
+      }
--- inferno-lsp/src/Inferno/LSP/Hover.hs
+++ inferno-lsp/src/Inferno/LSP/Hover.hs
@@ -1,1 +1,20 @@
-module Inferno.LSP.Hover where
+module Inferno.LSP.Hover
+  ( HoverIndex,
+    linearize,
+  ) where
+
+import Inferno.Types.Syntax (TCScheme, TypeMetadata)
+
+import Data.IntervalMap.Generic.Strict (IntervalMap)
+import Data.IntervalMap.Interval (Interval)
+import Text.Megaparsec.Pos (SourcePos)
+import qualified Text.Megaparsec.Pos as Pos
+
+type HoverIndex =
+  IntervalMap (Interval Int) (TypeMetadata TCScheme, SourcePos, SourcePos)
+
+-- | Encode a `SourcePos` as a single `Int` for use as an `IntervalMap` key.
+-- Uses a fixed column width of `10000`; safe for any realistic Inferno script.
+linearize :: Pos.SourcePos -> Int
+linearize pos =
+  (Pos.unPos pos.sourceLine - 1) * 10000 + (Pos.unPos pos.sourceColumn - 1)
